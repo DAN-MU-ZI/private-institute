7ccfedce2d51b5f19de07ce679e2d1feb187b296
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-06-07 01:31:59 +0900

    프로젝트 초기화 (.gitignore 적용)
    적용 키워드 - windows, macos, linux, java, gradle

---
+++ src/main/java/com/project/privateinstitute/PrivateInstituteApplication.java
@@ -0,0 +1,13 @@
+package com.project.privateinstitute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateInstituteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateInstituteApplication.class, args);
+	}
+
+}
